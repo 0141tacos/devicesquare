35b010676dc9d19b32f4671bbddbca6c8e8b8991
0141tacos <laggyboy@outlook.jp>
2025-03-16 11:01:57 +0900

    app.pyのDBに関する記述削除

--- app.py
+++ app.py
@@ -22,37 +22,6 @@ login_manager.init_app(app)
 
 init_api(app)
 
-# うまく動いたら削除する
-""""
-class Post(db.Model):
-    __tablename__ = 'post'
-    post_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50), nullable=False)
-    body = db.Column(db.String(400))
-    # 画像のカラムを作りたい
-    # いったん保留（画像の保管方法をDBにするか外部ストレージにするか決められないため）
-    # image = db.Column(db.blob)
-    url = db.Column(db.String(500))
-    created_at = db.Column(db.DateTime, nullable=False,
-                default=datetime.now(pytz.timezone('Asia/Tokyo')))
-    updated_at = db.Column(db.DateTime, nullable=False,
-                default=datetime.now(pytz.timezone('Asia/Tokyo')))
-    # 論理削除フラグ
-    # 20250227時点ではCRUDの勉強のため利用しないこととする
-    delete_flag = db.Column(db.Boolean, nullable=False, default=0)
-
-class User(UserMixin, db.Model):
-    __tablename__ = 'user'
-    user_id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(30), nullable=False, unique=True)
-    password = db.Column(db.String(12))
-    created_at = db.Column(db.DateTime, nullable=False,
-                default=datetime.now(pytz.timezone('Asia/Tokyo')))
-    # Flask-Loginでは"id"を定義しないといけないが、"user_id"としてしまったのでget_id()をオーバーライドしている
-    def get_id(self):
-        return str(self.user_id)
-"""
-
 # ログイン状態を管理するための関数
 @login_manager.user_loader
 def load_user(user_id):
